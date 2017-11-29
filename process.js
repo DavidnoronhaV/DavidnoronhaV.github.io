300e00fbf4f04004dea211e19cd7f3235ecc0942
David Noronha <32617923+DavidnoronhaV@users.noreply.github.com>
2017-11-29 19:59:12 +0530

    Update process.js

--- process.js
+++ process.js
@@ -1,7 +1,7 @@
 
   function setCookie(name, value)
   {
-    Cookies.set(name, value, { expires: 'Fri, 31 Dec 9999 23:59:59 GMT', path: '/' });
+    Cookies.set(name, value, { expires: 365 , path: '/' });
   }
 
   /**
